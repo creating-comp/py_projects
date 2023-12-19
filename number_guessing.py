efd6f2912ef9592b0d417a907cd04b3b7158bcd0
creating-comp <101798435+creating-comp@users.noreply.github.com>
2023-12-19 16:49:34 +0300

    Update number_guessing.py

--- number_guessing.py
+++ number_guessing.py
@@ -1,10 +1,3 @@
-'''
-Build a Number guessing game, in which the user selects a range.+
-Let’s say User selected a range, i.e., from A to B,+
-where A and B belong to Integer.+
-Some random integer will be selected by the system and+
-the user has to guess that integer in the minimum number of guesses+
-'''
 from random import randint
 a = int(input("Your range is from: "))
 b = int(input("To: "))
